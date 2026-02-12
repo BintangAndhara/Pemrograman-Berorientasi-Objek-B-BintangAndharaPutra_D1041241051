601f1cccdcf87750e95430034c832df3800180be
BintangAndhara <bintangandha1745@gmail.com>
2026-02-13 05:32:28 +0700

    Pertemuan 1

---
+++ Pertemuan1/BintangAndharaPutra_D1041241051_4.java
@@ -0,0 +1,12 @@
+public class BintangAndharaPutra_D1041241051_4 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Bintang Andhara Putra");
+        System.out.println("Nim : D1041241051");
+        System.out.println("Semester : 4");
+        System.out.println("Prodi : Informatika");
+    }
+
+//Nama : Bintang Andhara Putra
+//Nim : D1041241051
+//Hari/tangggal : Kamis,5 Febuari 2026
+}
---
+++ Pertemuan1/KodePertama.java
@@ -0,0 +1,10 @@
+public class KodePertama {
+    public static void main(String[] args) {
+        System.out.println("Halo, Pemrograman Berorientasi Objek");
+        System.out.println("ini adalah program Java pertama saya");
+    }
+
+//Nama : Bintang Andhara Putra
+//Nim : D1041241051
+//Hari/tangggal : Kamis,5 Febuari 2026
+}
